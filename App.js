6f9fa18827534de3f1d3c1848399f7e7dbb16b1e
Aditya Sinha <sinhadityaforever@gmail.com>
2022-09-06 19:26:29 +0530

    L5

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [name, setName] = useState('aditya');
+	const updateName = () => {
+		setName('Rahul');
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text>Naming lorem ipsum: {name}</Text>
+
+			<TextInput
+				style={styles.input}
+				placeholder="Enter name"
+				onChangeText={(e) => {
+					setName(e);
+				}}
+			></TextInput>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	header: {
+		backgroundColor: 'pink',
+		padding: 20
+	},
+	input: {
+		borderWidth: 1,
+		width: '50%',
+		borderColor: '#777',
+		padding: 8,
+		margin: 10
+	}
 });
